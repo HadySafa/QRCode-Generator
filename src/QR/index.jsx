ec05a2d34c38c81ba52c43fc645c6c48c2753287
Hady Safa <hadyabdallahsafa@gmail.com>
2024-11-10 13:22:28 +0200

    Added download button

--- src/QR/index.jsx
+++ src/QR/index.jsx
@@ -1,9 +1,10 @@
 import styles from './style.module.css'
 import QRCode from 'react-qr-code'
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import leftArrow from '../assets/leftArrow.png'
 
-function QR({choice,scan,setScan}) {
+
+function QR({ choice, scan, setScan }) {
 
     const choices = [
         {
@@ -28,9 +29,11 @@ function QR({choice,scan,setScan}) {
         }
     ]
 
-    
+
     const [QRValue, setQRValue] = useState("https://www.linkedin.com/in/hadyabdallahsafa/");
+    const [qrURL, setQrURL] = useState("");
     const input = useRef();
+    const qr = useRef();
 
     function generateCode() {
         if (input.current.value && input.current.value.trim() !== "") {
@@ -57,11 +60,22 @@ function QR({choice,scan,setScan}) {
         }
     }
 
+    useEffect(() => {
+        const svgElement = qr.current;
+        const svgString = new XMLSerializer().serializeToString(svgElement);
+        const blob = new Blob([svgString], { type: 'image/svg+xml' });
+        const url = URL.createObjectURL(blob);
+        setQrURL(url)
+    }, [scan])
+
     return (
         <section className={styles.container}>
 
             <div className={styles.qrContainer}>
-                <QRCode value={QRValue} bgColor='#eae0d5' fgColor='#22333b' />
+                <QRCode ref={qr} value={QRValue} bgColor='#eae0d5' fgColor='#22333b' />
+                {
+                    scan && qrURL ? <a className={styles.button} download="qr.svg" href={qrURL}>Download</a> : null
+                }
             </div>
 
             <div className={styles.inputContainer}>
